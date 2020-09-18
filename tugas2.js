4b5e35539c1ed4181bc8148db8646d48f1cd208e
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-09-18 10:47:05 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var nip = 12345678
+var nama = "Budiman Abdurrohman"
+var statusnikah = true
+var i = null
+var aray = ["satu", "dua", "tiga"]
+
+console.log (nip)
+console.log (nama)
+console.log (statusnikah)
+console.log (i)
+console.log (aray)
